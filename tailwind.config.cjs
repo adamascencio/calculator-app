28424e049fafd8931c17649300f700a55b71dee9
adamascencio <ascencioadam@gmail.com>
2023-02-24 20:16:57 -0800

    add color theme and set background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      // backgrounds
+      main: 'hsl(222, 26%, 31%)',
+      keypad: 'hsl(223, 31%, 20%)',
+      screen: 'hsl(224, 36%, 15%)',
+      // keys
+      keyBg: 'hsl(225, 21%, 49%)',
+      keyShadow: 'hsl(224, 28%, 35%)',
+      keyRed: 'hsl(6, 63%, 50%)',
+      keyDarkRed: 'hsl(6, 70%, 34%)',
+      keyLightGray: 'hsl(30, 25%, 89%)',
+      keyGrayOrange: 'hsl(28, 16%, 65%)',
+      // text
+      darkGray: 'hsl(221, 14%, 31%)',
+      white: 'hsl(0, 0%, 100%)',
+    }
   },
   plugins: [],
 }
