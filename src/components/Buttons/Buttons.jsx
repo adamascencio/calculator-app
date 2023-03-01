897620f80ddf5ce3c1b6718db831576cf77ba232
adamascencio <ascencioadam@gmail.com>
2023-03-01 13:45:43 -0800

    add logic to handle calculating total

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,46 +1,69 @@
-export default function Buttons ({ displayNum, setDisplayNum, calcArr, setCalcArr, setCount, setOperator }) {
-  // event handlers
-  const handleNumChange = (e) => {
-    const num = e.target.innerText;
-    setDisplayNum(formatStr(displayNum + num));
+import { useEffect } from 'react';
+
+export default function Buttons ({ displayNum, setDisplayNum, calcArr, setCalcArr, result, setResult }) {
+  // constants
+  const OPERATORS = ['+', '-', '/', '*'];
+
+  // useEffect
+  useEffect(() => {
+    const lastIdx = calcArr.length - 1;
+    OPERATORS.includes(calcArr[lastIdx]) ?
+      setResult(eval(calcArr.slice(0, -1).join('')))
+      : 
+      setResult(eval(calcArr.join('')))
+    setDisplayNum(result ? result.toString() : '');
+  }, [calcArr]);
+
+  // helper functions
+  const checkForDecimal = (numStr) => {
+    const regex = /[.]+/g;
+    return regex.test(numStr);
   }
 
-  const handleOperator = (e) => {
-    const operator = e.target.innerText;
-    const lastNum = calcArr[calcArr.length - 1];
-    setOperator(operator);
-    setCalcArr([...calcArr, displayNum]);
-    setCount(compute(operator, calcArr.length > 1 ? lastNum : displayNum));
-    setDisplayNum(count.toString());
+  // event handlers
+  const handleNumChange = (e) => {
+    const char = e.target.value;
+    const lastChar = displayNum[displayNum.length - 1];
+    if (
+        OPERATORS.includes(char) && displayNum === '' ||
+        OPERATORS.includes(char) && OPERATORS.includes(lastChar) ||
+        char === '.' && checkForDecimal(displayNum)
+    ) return;
+
+    if (OPERATORS.includes(char)) {
+      setCalcArr([...calcArr, displayNum, char]) 
+      setDisplayNum('');
+    }
+    
+    if (!OPERATORS.includes(char)) setDisplayNum(displayNum + char);
   }
 
   const handleReset = () => {
     setDisplayNum('');
     setCalcArr([]);
-    setOperator('');
-    setCount(0);
+    setResult(0);
   }
 
   return (
     <div id='num-buttons' className='grid grid-cols-4 text-black font-bold place-items-center justify-items-stretch gap-3 bg-keypad p-4'>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>7</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>8</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>9</button>
-      <button className='bg-keyBg text-white'>DEL</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>4</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>5</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>6</button>
-      <button onClick={handleOperator} className='bg-keyLightGray'>+</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>1</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>2</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>3</button>
-      <button onClick={handleOperator} className='bg-keyLightGray'>-</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>.</button>
-      <button onClick={handleNumChange} className='bg-keyLightGray'>0</button>
-      <button onClick={handleOperator} className='bg-keyLightGray'>/</button>
-      <button onClick={handleOperator} className='bg-keyLightGray'>x</button>
-      <button onClick={handleReset} className='col-span-2 bg-keyBg text-white'>RESET</button>
-      <button className='col-span-2 bg-keyRed text-white'>=</button>
+      <button value='7' onClick={handleNumChange} className='bg-keyLightGray'>7</button>
+      <button value='8' onClick={handleNumChange} className='bg-keyLightGray'>8</button>
+      <button value='9' onClick={handleNumChange} className='bg-keyLightGray'>9</button>
+      <button value='DEL' className='bg-keyBg text-white'>DEL</button>
+      <button value='4' onClick={handleNumChange} className='bg-keyLightGray'>4</button>
+      <button value='5' onClick={handleNumChange} className='bg-keyLightGray'>5</button>
+      <button value='6' onClick={handleNumChange} className='bg-keyLightGray'>6</button>
+      <button value='+' onClick={handleNumChange} className='bg-keyLightGray'>+</button>
+      <button value='1' onClick={handleNumChange} className='bg-keyLightGray'>1</button>
+      <button value='2' onClick={handleNumChange} className='bg-keyLightGray'>2</button>
+      <button value='3' onClick={handleNumChange} className='bg-keyLightGray'>3</button>
+      <button value='-' onClick={handleNumChange} className='bg-keyLightGray'>-</button>
+      <button value='.' onClick={handleNumChange} className='bg-keyLightGray'>.</button>
+      <button value='0' onClick={handleNumChange} className='bg-keyLightGray'>0</button>
+      <button value='/' onClick={handleNumChange} className='bg-keyLightGray'>/</button>
+      <button value='*' onClick={handleNumChange} className='bg-keyLightGray'>x</button>
+      <button value='RESET' onClick={handleReset} className='col-span-2 bg-keyBg text-white'>RESET</button>
+      <button value='=' className='col-span-2 bg-keyRed text-white'>=</button>
     </div>
   );
 }
