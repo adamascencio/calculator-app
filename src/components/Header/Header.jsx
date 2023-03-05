c0a16512308cb125f678705e3fc9b80af3655734
adamascencio <ascencioadam@gmail.com>
2023-03-05 12:44:17 -0800

    add slider and bind value to theme state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
+
 export default function Header () {
+  const [theme, setTheme] = useState('1');
+
   return (
     <header className='flex justify-between items-center'>
       <h1 className='font-bold text-2xl'>calc</h1>
       <div id="theme-selector" className='flex gap-4 items-center'>
         <h2 className='font-bold ml-12'>Theme</h2>
-        <div className='flex gap-x-4 bg-keypad px-4 py-2 rounded-lg '>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+        <div className='flex gap-4 bg-keypad px-2 py-1 rounded-lg w-16 relative'>
+          <input 
+            type='range'
+            min='1'
+            max='3'
+            value={theme}
+            className='cursor-pointer rounded-full h-3'
+            onChange={e => setTheme(e.target.value)}
+          > 
+          </input>
         </div>
       </div>
     </header>
