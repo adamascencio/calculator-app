868412371ddfe200a54d87b61761e53363930bc7
adamascencio <ascencioadam@gmail.com>
2023-03-08 10:22:01 -0800

    adjust slider and label width

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,20 +6,20 @@ export default function Header () {
   return (
     <header className='flex justify-between items-center'>
       <h1 className='font-bold text-2xl'>calc</h1>
-      <div className='absolute flex gap-3 right-9 top-3 text-sm font-bold'>
+      <div className='absolute flex gap-4 right-10 top-3 text-sm font-bold'>
         <span>1</span>
         <span>2</span>
         <span>3</span>
       </div>
       <div id="theme-selector" className='flex gap-4 items-center'>
         <h2 className='font-bold ml-12'>Theme</h2>
-        <div className='flex gap-4 bg-keypad px-2 py-1 rounded-lg w-17'>
+        <div className='flex bg-keypad px-2 py-1 rounded-lg w-17'>
           <input 
             type='range'
             min='1'
             max='3'
             value={theme}
-            className='cursor-pointer bg-transparent w-14'
+            className='cursor-pointer bg-transparent w-[70px]'
             onChange={e => setTheme(e.target.value)}
           > 
           </input>
