87fb8907d4d5d5f7338fced947ad4bbb3e67e42c
adamascencio <ascencioadam@gmail.com>
2023-03-30 09:42:28 -0700

    Update tailwind.config.cjs with removed background colors and text colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,20 +16,8 @@ module.exports = {
         equalsKey: 'var(--equalsKey)',
         textColor: 'var(--textColor)',
         opKeyColor: 'var(--opKeyColor)',
-        // backgrounds
-        main: 'hsl(222, 26%, 31%)',
-        keypad: 'hsl(223, 31%, 20%)',
-        screen: 'hsl(224, 36%, 15%)',
-        // keys
-        keyBg: 'hsl(225, 21%, 49%)',
-        keyShadow: 'hsl(224, 28%, 35%)',
         keyRed: 'hsl(6, 63%, 50%)',
         keyDarkRed: 'hsl(6, 70%, 34%)',
-        keyLightGray: 'hsl(30, 25%, 89%)',
-        keyGrayOrange: 'hsl(28, 16%, 65%)',
-        // text
-        darkGray: 'hsl(221, 14%, 31%)',
-        white: 'hsl(0, 0%, 100%)',
       },
       fontSize: {
         keySize: 'clamp(1rem, 6vw, 2rem)',
